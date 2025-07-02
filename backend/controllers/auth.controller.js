d0eba17928074ab948145752d264b037e5aba4b2
Angel <acalmenc@gmail.com>
2025-07-02 14:33:13 -0400

    Implement user registration, login, and token refresh functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,81 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/index.js";
 import { generateAccessToken, generateRefreshToken, verifyRefreshToken } from "../utils/jwt.js";
+
+export const register = async (req, res) => {
+    try {
+        const {username, email, password, role} = req.body;
+
+        const existing = await User.findOne({ where: {email} });
+        if (existing) {
+            return res.status(400).json({message: "El usuario ya existe"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+            role: role || "user"
+        });
+        res.status(201).json({message: "Usuario registrado"});
+    } catch (error) {
+        console.error("Error en registro:", error);
+        res.status(500).json({message: "Error en el servidor"});
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ where: {email} });
+        if (!user) {
+            return res.status(400).json({message: "Credenciales inválidas"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: "Credenciales inválidas"});
+        }
+
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        res.status(200).json({
+            accessToken,
+            refreshToken,
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                role: user.role
+            }
+        })
+    } catch (error) {
+        console.error("Error en login:", error);
+        res.status(500).json({message: "Error en el servidor"});
+    }
+}
+
+export const refreshToken = (req, res) => {
+    try {
+        const { token } = req.body;
+        if (!token) {
+            return res.status(401).json({ message: "Token requerido" });
+        }
+
+        const payload = verifyRefreshToken(token);
+        const user = { id: payload.id }; // Solo necesitamos el ID para generar un nuevo access token
+        if (!user) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        const newAccessToken = generateAccessToken(user);
+
+        res.status(200).json({ accessToken: newAccessToken });
+    } catch (error) {
+        console.error("Error en refresh token:", error);
+        res.status(403).json({ message: "Token inválido" });
+    }
+}
